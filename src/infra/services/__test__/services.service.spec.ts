d81abc54b9fd389afa456400a1d3d0e155b2fc76
Cristian <cristian.jimenezsa@hotmail.com>
2024-10-22 15:04:31 -0500

    services service test suite improved

--- src/infra/services/__test__/services.service.spec.ts
+++ src/infra/services/__test__/services.service.spec.ts
@@ -125,4 +125,104 @@ describe('ServicesService', () => {
       expect(mockServicesRepository.create).toHaveBeenCalledTimes(1);
     });
   });
+
+  describe('update', () => {
+    it('should throw a InternalServerErrorException', async () => {
+      const updateData = { name: 'Amazing' };
+      mockServicesRepository.update.mockRejectedValue(Error('Database Error'));
+      await expect(service.update(1, updateData)).rejects.toThrow(
+        InternalServerErrorException,
+      );
+      expect(mockServicesRepository.update).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw a NotFoundException', async () => {
+      const updateData = { name: 'Amazing' };
+      mockServicesRepository.findOne.mockResolvedValue(null);
+      await expect(service.update(1, updateData)).rejects.toThrow(
+        NotFoundException,
+      );
+      expect(mockServicesRepository.findOne).toHaveBeenCalledTimes(1);
+    });
+
+    it('should update a service', async () => {
+      const updateData = { name: 'Amazing' };
+      const result = 'Service updated successfully';
+      mockServicesRepository.findOne.mockResolvedValue(new Service());
+      mockServicesRepository.update.mockResolvedValue(new Service());
+      await expect(service.update(1, updateData)).resolves.toEqual(result);
+      expect(mockServicesRepository.update).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('softDelete', () => {
+    it('should throw a InternalServerErrorException', async () => {
+      mockServicesRepository.softDelete.mockRejectedValue(
+        Error('Database Error'),
+      );
+      await expect(service.softDelete(1)).rejects.toThrow(
+        InternalServerErrorException,
+      );
+      expect(mockServicesRepository.softDelete).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw a NotFoundException', async () => {
+      mockServicesRepository.softDelete.mockResolvedValue({ affected: 0 });
+      await expect(service.softDelete(1)).rejects.toThrow(NotFoundException);
+      expect(mockServicesRepository.softDelete).toHaveBeenCalledTimes(1);
+    });
+
+    it('should soft delete a service', async () => {
+      const result = 'Service deleted successfully';
+      mockServicesRepository.softDelete.mockResolvedValue({ affected: 1 });
+      await expect(service.softDelete(1)).resolves.toEqual(result);
+      expect(mockServicesRepository.softDelete).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('restore', () => {
+    it('should throw a InternalServerErrorException', async () => {
+      mockServicesRepository.restore.mockRejectedValue(Error('Database Error'));
+      await expect(service.restore(1)).rejects.toThrow(
+        InternalServerErrorException,
+      );
+      expect(mockServicesRepository.restore).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw a NotFoundException', async () => {
+      mockServicesRepository.restore.mockResolvedValue({ affected: 0 });
+      await expect(service.restore(1)).rejects.toThrow(NotFoundException);
+      expect(mockServicesRepository.restore).toHaveBeenCalledTimes(1);
+    });
+
+    it('should restore a service', async () => {
+      const result = 'Service restored successfully';
+      mockServicesRepository.restore.mockResolvedValue({ affected: 1 });
+      await expect(service.restore(1)).resolves.toEqual(result);
+      expect(mockServicesRepository.restore).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getServiceOrThrow', () => {
+    it('should throw a NotFoundException', async () => {
+      mockServicesRepository.findOne.mockResolvedValue(null);
+      await expect(service.getServiceOrThrow(1)).rejects.toThrow(
+        NotFoundException,
+      );
+      expect(mockServicesRepository.findOne).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return a service by ID', async () => {
+      const result = {
+        id: 1,
+        name: 'Amazing',
+        description: 'description',
+        cost: 100,
+        category: ServiceCategory.ENTERTAINMENT,
+      };
+      mockServicesRepository.findOne.mockResolvedValue(result);
+      await expect(service.getServiceOrThrow(1)).resolves.toEqual(result);
+      expect(mockServicesRepository.findOne).toHaveBeenCalledTimes(1);
+    });
+  });
 });
